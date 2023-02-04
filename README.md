dfee2b8a0fb9b3f7919a4556ac67d4e4fe087de3
Q16solver <50786287+Q16solver@users.noreply.github.com>
2023-02-04 11:54:13 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nuxt-str.replace-repro
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-wn9aqn)
---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <template #prepend>Hello</template>
+      <template #append>World</template>
+    </v-app-bar>
+  </v-app>
+</template>
